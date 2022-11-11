7e2801645cf446108af539a337ca85b7ec0f2d42
snall6 <snall6@illinois.edu>
2022-11-11 16:33:07 +0000

    made map representing data

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <fstream>
 #include <iostream>
 #include <iomanip>
+#include <map>
 using namespace std;
 
 int main() {
     int sum = 0;
+    int index = 0;
+    int key;
     int x;
     ifstream inFile;
     inFile.open("roadNet-TX.txt");
@@ -12,11 +15,22 @@ int main() {
         std::cout << "Unable to open file";
         exit(1); // terminate with error
     }
+    map<int, int> connections;
     while (inFile >> x) {
-        std::cout << x << std::endl;
+        //std::cout << x << std::endl;
+        if (index % 2 == 0) {
+            key = x;
+        } else if (index % 2 == 1) {
+            connections.insert(make_pair(key, x));
+        }
+        index++;
     }
     inFile.close();
     std::cout << "Sum = " << sum << std::endl; 
-
+    // for (auto connection : connections) {
+    //     std::cout << "From: " << connection.first << " To: " << connection.second << std::endl;
+    // }
+    std::cout << connections.size() << std::endl;
     return 0;
 }
+
