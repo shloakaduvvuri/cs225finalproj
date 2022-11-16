46a1123f2ef09442fabfcb46d31066ac771f6e31
snall6 <snall6@illinois.edu>
2022-11-16 21:44:13 +0000

    made map representing graph - int to vector<int>

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include <iomanip>
 #include <map>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    int sum = 0;
     int index = 0;
+    //int sum = 0;
     int key;
     int x;
     ifstream inFile;
@@ -15,22 +17,38 @@ int main() {
         std::cout << "Unable to open file";
         exit(1); // terminate with error
     }
-    map<int, int> connections;
+    map<int, vector<int>> graph_;
     while (inFile >> x) {
         //std::cout << x << std::endl;
         if (index % 2 == 0) {
             key = x;
+            if (graph_.find(key) == graph_.end()) {
+                vector<int> vals;
+                graph_.insert(make_pair(key, vals));
+            }
         } else if (index % 2 == 1) {
-            connections.insert(make_pair(key, x));
+          graph_[key].push_back(x);  
+            
         }
         index++;
     }
     inFile.close();
-    std::cout << "Sum = " << sum << std::endl; 
-    // for (auto connection : connections) {
-    //     std::cout << "From: " << connection.first << " To: " << connection.second << std::endl;
+    // std::cout << graph_.size() << std::endl;
+    // std::cout << graph_.at(1).size() << std::endl;
+
+    for (auto node_relation : graph_) {
+        sort(node_relation.second.begin(), node_relation.second.end());
+    }
+
+    // for (auto node_relation : graph_) {
+    //     std::cout << "City " << node_relation.first << " Connects To: " << std::endl;
+    //     std::cout << "{ ";
+    //     for (int neighbor : node_relation.second) {
+    //         std::cout << neighbor << " ";
+    //     }
+    //     std::cout << "}" << std::endl;
     // }
-    std::cout << connections.size() << std::endl;
-    return 0;
+    // std::cout << "}" << std::endl;
+    return 0;   
 }
 
