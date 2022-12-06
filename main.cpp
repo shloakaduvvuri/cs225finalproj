5753a782c62526efa4af07fad5cfa47aa974887e
Shloaka Duvvuri <shloakaduvvuri@gmail.com>
2022-12-06 03:57:12 +0000

    fixed bfs

--- main.cpp
+++ main.cpp
@@ -4,22 +4,21 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 int main() {
     int index = 0;
-    //int sum = 0;
     int key;
     int x;
     ifstream inFile;
     inFile.open("roadNet-TX.txt");
     if (!inFile) {
         std::cout << "Unable to open file";
-        exit(1); // terminate with error
+        exit(1);
     }
     map<int, vector<int>> graph_;
     while (inFile >> x) {
-        //std::cout << x << std::endl;
         if (index % 2 == 0) {
             key = x;
             if (graph_.find(key) == graph_.end()) {
@@ -33,21 +32,46 @@ int main() {
         index++;
     }
     inFile.close();
-    // std::cout << graph_.size() << std::endl;
-    // std::cout << graph_.at(1).size() << std::endl;
 
     for (auto node_relation : graph_) {
         sort(node_relation.second.begin(), node_relation.second.end());
     }
-    for (auto node_relation : graph_) {
-    //     std::cout << "City " << node_relation.first << " Connects To: " << std::endl;
-    //     std::cout << "{ ";
-        //for (int neighbor : node_relation.second) {
-            //std::cout << neighbor << " ";
-        //}
-    //     std::cout << "}" << std::endl;
+
+    //BFS TRAVERSAL
+    queue<int> q; 
+    vector<int> visited;
+    int from = 0; 
+    //int arrive = 1; 
+    q.push(from);
+    visited.push_back(from);
+    //int count = 0;
+    //bool finished = false;
+    while (!q.empty()) {
+        int curr = q.front();
+        q.pop();
+        vector<int> tovisit = graph_.at(curr);
+        for (unsigned i = 0; i < tovisit.size(); i++) {
+            if (find(visited.begin(), visited.end(), tovisit.at(i)) == visited.end()) {
+                /*if (tovisit.at(i) == arrive) {
+                    count++;
+                    std::cout << "Path Length: " << count << std::endl;
+                    finished = true;
+                    break;
+                }*/
+                q.push(tovisit.at(i));
+                visited.push_back(tovisit.at(i));
+                //count++;
+            }
+        }
+        /*if (finished) {
+            break;
+        }*/
     }
-    // std::cout << "}" << std::endl;
+    for (int v : visited) {
+        std::cout << v << " ";
+    }
+    std::cout << '\n';
+
     return 0;   
 }
 
