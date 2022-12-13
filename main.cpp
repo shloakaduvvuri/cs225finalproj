9b60f5f212bd264ff39d205715d4f37b2b759adb
srkethireddy <srk9@illinois.edu>
2022-12-13 02:44:16 +0000

    eulerian shreshta+deepika

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <cassert>
 #include <queue>
 #include <climits>
+#include <stack>
 #include <set>
 
 using namespace std;
@@ -33,7 +34,7 @@ int main() {
                 graph_.insert(make_pair(key, vals));
             }
         } else if (index % 2 == 1) {
-          graph_[key].push_back(x);   
+          graph_[key].push_back(x);  
         }
         index++;
     }
@@ -56,7 +57,7 @@ int main() {
     //bfs
     cout << "Breadth First Search Traversal" << endl;
     int from = 0;
-    queue<int> q; 
+    queue<int> q;
     set<int> visited;
     q.push(from);
     //visited.push_back(from);
@@ -80,7 +81,7 @@ int main() {
     //djikstras
     cout << "Djikstra's Shortest Path - Closest 100 Nodes to Source" << endl;
     int source = 0;
-    int count = 0; 
+    int count = 0;
     vector<pair<int, int>> distances;
     set<int> already_seen;
     distances.push_back(make_pair(source, count));
@@ -91,7 +92,7 @@ int main() {
         int curr = next_node.front();
         next_node.pop();
         vector<int> neighbors = graph_.at(curr);
-        count++; 
+        count++;
         for (auto neighbor : neighbors) {
             if (already_seen.find(neighbor) == already_seen.end()) {
                 already_seen.insert(neighbor);
@@ -106,8 +107,72 @@ int main() {
     }
 
     std::cout << '\n';
-    
+   
     //euler-path
     cout << "Eulerian Path - Add Specificity" << endl;
+     bool isConnected = true;
+    set<int> haveVisited;
+  
+    int idx=0;
+    bool done =false;
+   
+    for(auto elem:graph_){
+    
+        if(graph_.at(elem.first).size() > 0&&(!done)){
+            idx=elem.first;
+            done=true;
+        }
+    }
+ 
+   
+    stack<int> s;
+    s.push(idx);
+    while (!s.empty()) {
+        int c = s.top();
+        
+        s.pop();
+        vector<int> toVisit = graph_.at(c);
+        for (auto i : toVisit) {
+            if (haveVisited.find(i) == haveVisited.end()) {
+                s.push(i);
+                haveVisited.insert(i);
+               
+            }
+        }
+    }
+    //end dfs
+    if(haveVisited.size()!=graph_.size()){
+        isConnected=false;
+    }
+ 
+    bool isEulerian=true;
+    //end isconnection
+    //start iseulerian
+    if (isConnected == false){
+        std::cout<<"NOT EULERIAN BECAUSE NOT CONNECTED"<<std::endl;
+        isEulerian=false;
+    }
+    int odd = 0;
+    set<int>::iterator itr;
+    for(itr=haveVisited.begin();itr!=haveVisited.end();itr++){
+            if(graph_.at(*itr).size()%2==1){
+                odd++;
+            }
+
+    }
+   
+   
+    if(odd>2){
+        std::cout<<"NOT EULERIAN BECAUSE MORE THAN TWO NODES OF ODD DEGREE"<<std::endl;
+        isEulerian=false;
+    } else if(odd==0){
+        std::cout<<"EULERIAN CIRCUIT"<<std::endl;
+    } else{
+        std::cout<<"EULERIAN PATH"<<std::endl;
+    }
+ 
+  
+
+    
 
 }
